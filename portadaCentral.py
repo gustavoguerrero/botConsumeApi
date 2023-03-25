af25a15aab6e844e1918eab457e0cd4bf68e2e73
veoDoble <gustavoguerrerom@gmail.com>
2023-03-24 23:46:04 -0300

    agrego link sin mandar msg

--- portadaCentral.py
+++ portadaCentral.py
@@ -16,37 +16,40 @@ def parsearURL(urlp):
     items = soup.find_all("div", class_="sprocket-features-padding")
     
     for item in items:
+    
         viejo = False   
-        
+
         now = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
         now = datetime.strptime(now, '%Y-%m-%d %H:%M:%S')
 
-        yesterday = datetime.today() - timedelta(days=1)
+        yesterday = datetime.today() - timedelta(days=3)
         yesterday = yesterday.strftime('%Y-%m-%d %H:%M:%S')
         yesterday = datetime.strptime(yesterday, '%Y-%m-%d %H:%M:%S')
 
         try:
-            dateList = item.find_all("div", class_="date")[0].text.split("-")[0]
-            time = item.find_all("div", class_="date")[0].text.split("|")[1]
-            print(dateList)
-            date = f'{dateList[0]} {dateList[1]} {dateList[2]}'
-            print(date)
-            monthNumber = Utils.monthTranslate(dateList[1].lower())
+            dateList = item.find_all("div", class_="date")[0].text.split("|")[0].strip().split()
+            time = item.find_all("div", class_="date")[0].text.split("|")[1].strip()
+            date = f'{dateList[0]} {dateList[2]} {dateList[4]}'
+            
+            monthNumber = Utils.monthTranslate(dateList[2].lower())
 
             fecha = f'{dateList[4]}-{monthNumber}-{dateList[0]} {time}'
-            print(fecha)
+            
             fecha = datetime.strptime(fecha, '%Y-%m-%d %H:%M')
+            
             if (fecha >= yesterday and fecha <=now):
 
                 title = item.a.text.strip()
-                link = centralURL + item.find_all("a")[0]["href"]
+                link = centralURL + item.find_all("a", class_='px-readon')[0]["href"]
                 excerpt = item.find_all("div", class_="catItemIntroText")[0].text.strip()
-                
+                print(link)
                 sendMessage(f'Publicado por @affur_bot\nTomado de la web del PIT-CNT\n\n{link}')
-                time.sleep(15)
+                
+
             else:
                 viejo = True
-            
+                print(viejo)
+
         except IndexError:
             pass
         
@@ -56,7 +59,9 @@ def parsearURL(urlp):
             'chat_id': id_admin,
             'text' : f'{now}\n\ncentral No Actualizada'
         })
+
 def sendMessage(message):
+    print(id_admin)
     
     requests.post(url, data = {
         'chat_id': id_admin, #id_channel,
